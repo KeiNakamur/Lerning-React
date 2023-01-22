dac18c4bc9ff2f575fe43693224ab9096875c699
KeiNakamura <ktyun2525@gmail.com>
2023-01-22 11:47:09 +0900

    learned recoil

---
+++ lerning-recoil/src/components/addTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div className='taskList'>
+      <div>５個のタスクがあります</div>
+      <ul>
+        <li>初めてのタスク</li>
+      </ul>
+    </div>
+
+  )
+}
+
+export default AddTask
